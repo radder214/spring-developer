b3f6743598cefde194a3a68f18f6f1609ed6b205
terious <garden940316@gmail.com>
2024-07-21 19:49:02 +0900

    comment

--- src/test/java/JUnitQuiz3.java
+++ src/test/java/JUnitQuiz3.java
@@ -3,6 +3,8 @@ import org.junit.jupiter.api.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+// 1. 각각의 테스트를 실행하기 전에 “Hello!” 출력
+// 2. 모든 테스트가 끝나면 “Bye!” 출력
 public class JUnitQuiz3 {
     @DisplayName("@BeforeEach") // 해당 annotation 이 붙어도 콘솔창에 출력 안 됨
     @BeforeEach
