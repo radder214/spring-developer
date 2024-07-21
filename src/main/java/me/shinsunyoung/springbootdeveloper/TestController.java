dd9aac22a55175ab4e636b987b04c2084d8ec6ed
terious <garden940316@gmail.com>
2024-07-21 17:12:10 +0900

    first commit

---
+++ src/main/java/me/shinsunyoung/springbootdeveloper/TestController.java
@@ -0,0 +1,28 @@
+package me.shinsunyoung.springbootdeveloper;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+//@RequestMapping("/test")
+public class TestController {
+    @Autowired
+    TestService testService;
+
+    @GetMapping("/test")
+    public List<Member> getAllMembers() {
+        List<Member> members = testService.getAllMembers();
+        return members;
+    }
+
+    @GetMapping("/test2")
+    public String test() {
+        return "Hello World";
+    }
+
+
+}
